af19754433389688db94c4234e2de98cd97bc83e
Tpessia <30393840+Tpessia@users.noreply.github.com>
2017-12-24 16:54:13 -0200

    offline remove

--- assets/js/home.js
+++ assets/js/home.js
@@ -10,8 +10,7 @@ $(function() {
     // window.addEventListener('online', function () { newData('1', '1I5avuVF1MCJyDQAEk9lrflQsuA4q6wWoMiVqO6pKiT0'); });
 
     // window.addEventListener('offline', function () { offline(); });
-    Offline.check();
-    console.log(Offline.state);
+    
     if (/*typeof Offline !== "undefined" && Offline.state == "up"*/ navigator.onLine) {
         newData('1', '1I5avuVF1MCJyDQAEk9lrflQsuA4q6wWoMiVqO6pKiT0');
         Offline.on("down", function () {
