1d994deae385a6fb16a3fd89f7ae42746ac0000f
Tpessia <30393840+Tpessia@users.noreply.github.com>
2018-01-16 11:21:01 -0200

    back just in case

--- assets/js/home.js
+++ assets/js/home.js
@@ -192,7 +192,6 @@ charts1 = {
                         label: "Pontuação Média",
                         backgroundColor: 'rgba(0, 0, 0, 0)',
                         borderColor: 'rgb(0,0,0)',
-                        hidden: true,
                         // data: [0, 25, 40, 50, 90, 100, 125, 145, 180, 180, 200, 230],
                     },
 
@@ -200,7 +199,6 @@ charts1 = {
                         label: "Minha Pontuação",
                         backgroundColor: 'rgba(255, 171, 64, 0.7)',
                         borderColor: 'rgb(255, 171, 64)',
-                        hidden: true,
                         // data: [0, 10, 15, 20, 20, 30, 50, 100, 125, 125, 200, 230]
                     }
                 ]
@@ -227,13 +225,7 @@ charts1 = {
     change: function (datasetIndex, dataArr, labelsArr) {
         chart1.data.datasets[datasetIndex].data = dataArr;
         chart1.data.labels = labelsArr;
-        scrollFireCharts("#myChart", function () {
-            for (var i in chart1.data.datasets) {
-                chart1.data.datasets[i].hidden = false;
-            }
-            chart1.update();
-        }, 1);
-        // chart1.update();
+        chart1.update();
     }
 }
 
@@ -265,7 +257,6 @@ charts2 = {
                         // backgroundColor: '#25365d66',
                         backgroundColor: 'rgba(0,0,0,0)',
                         borderColor: '#25365d',
-                        hidden: true,
                         // data: [5, 2, 4, 5, 9, 10, 5, 4, 8, 8, 2, 3]
                     }
                 ]
@@ -296,13 +287,7 @@ charts2 = {
     change: function (datasetIndex, dataArr, labelsArr) {
         chart2.data.datasets[datasetIndex].data = dataArr;
         chart2.data.labels = labelsArr;
-        scrollFireCharts("#myChart2", function () {
-            for (var i in chart2.data.datasets) {
-                chart2.data.datasets[i].hidden = false;
-            }
-            chart2.update();
-        }, 2);
-        // chart2.update();
+        chart2.update();
     }
 }
 
@@ -323,7 +308,6 @@ charts3 = {
                         label: "Primeiros Colocados",
                         // backgroundColor: '#25365d66',
                         backgroundColor: 'rgba(11,21,43,0.9)',
-                        hidden: true,
                         // borderColor: '#25365d',
                         // data: [5, 2, 4, 5, 9, 10, 5, 4, 8, 8, 2, 3]
                     }
@@ -355,13 +339,7 @@ charts3 = {
     change: function (datasetIndex, dataArr, labelsArr) {
         chart3.data.datasets[datasetIndex].data = dataArr;
         chart3.data.labels = labelsArr;
-        scrollFireCharts("#myChart3", function () {
-            for (var i in chart3.data.datasets) {
-                chart3.data.datasets[i].hidden = false;
-            }            
-            chart3.update();
-        }, 3);
-        // chart3.update();
+        chart3.update();
     }
 }
 
@@ -469,25 +447,6 @@ function scrolls() {
     scrollFire('#rankContent>div:first-child', function () {
         $('#first').toggleClass('transform');
     }, 2);
-}
-
-scrollChart = [];
-function scrollFireCharts(selector, foo, id) {
-    scrollChart[id] = 0;
-
-    $(window).scroll(function () {
-        if (scrollChart[id] == 0) {
-            var sT = $(selector).offset().top,
-                sH = $(selector).outerHeight(),
-                wH = $(window).height(),
-                wS = $(window).scrollTop();
-
-            if (wS + wH > sT + sH) { //trigger quando o ranking volta a estar visível
-                foo();
-                scrollChart[id] = 1;
-            }
-        }
-    });
 }
 
 
