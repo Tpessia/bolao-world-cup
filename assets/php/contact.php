9a0a0c5bd317b619784cf3a93f700ee6fc787788
Tpessia <30393840+Tpessia@users.noreply.github.com>
2018-02-19 09:16:39 -0300

    test fix

--- assets/php/contact.php
+++ assets/php/contact.php
@@ -4,7 +4,7 @@
 </head>
 <body>
     <?php
-        header("Content-type: text/html; charset=utf-8");
+        // header("Content-type: text/html; charset=utf-8");
         error_reporting(-1);
         ini_set('display_errors', 'On');
         set_error_handler("var_dump");
@@ -15,9 +15,7 @@
         $name = $_POST["nome"];
         $subject = $_POST["assunto"] . " - " . $name;
         $txt = $_POST["mensagem"] . "<br><p>Enviada em: " . date('Y-m-d H:i:s') . " </p>";
-        $headers = "From: " . $_POST["email"];
-        $headers .= "Content-Type: text/html; charset=UTF-8";
-        $headers .= "Content-Transfer-Encoding: 8bit";
+        $headers = "From: " . $_POST["email"] . "\r\n" . "Content-Type: text/html; charset=UTF-8";
     
         $status = mail($dest,$subject,$txt,$headers);
 
