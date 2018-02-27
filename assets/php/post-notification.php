25b513d1e8c7ecf8f96f86a4d4eb6ee3e42efff1
Tpessia <30393840+Tpessia@users.noreply.github.com>
2018-02-27 08:15:56 -0300

    new pushpad

--- assets/php/post-notification.php
+++ assets/php/post-notification.php
@@ -4,8 +4,8 @@
     error_reporting(E_ALL);
     require_once('/home/u662107342/vendor/autoload.php');
     
-    Pushpad\Pushpad::$auth_token = '07071ad1707106950ecf1927a30038da';
-    Pushpad\Pushpad::$project_id = 5053; # set it here or pass it as a param to methods later
+    Pushpad\Pushpad::$auth_token = 'e3d31aa87f2d0131ae32a69bd3049b09';
+    Pushpad\Pushpad::$project_id = 5110; # set it here or pass it as a param to methods later
     echo 'ok';
 
     /*$notification = new Pushpad\Notification(array(
