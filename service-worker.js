708620935c3ceb9f49b07597d8f9de354e5ac62f
Tpessia <30393840+Tpessia@users.noreply.github.com>
2018-04-13 23:43:41 -0300

    sw

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 importScripts('/lib/cache-polyfill.js');
 
-var version = 'v0.6.2.1::';
+var version = 'v0.6.3::';
 
 self.addEventListener("install", function (event) {
     console.log('WORKER: install event in progress.');
