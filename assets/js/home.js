bd7d7564b758092cce85368a388ba0081915017a
Tpessia <30393840+Tpessia@users.noreply.github.com>
2018-04-14 18:31:40 -0300

    typo

--- assets/js/home.js
+++ assets/js/home.js
@@ -447,7 +447,7 @@ function RankCreate() { //create ranking element and object
         if (i < 5) { //create side rank
             sideRank += '<div class="card"><div class="card-content white-text row"><span class="card-title col s8 nome">' + (parseInt(i) + 1) + ". " + ranking[i].name + '</span><span class="card-action col s3 verMais"><a class="btn-floating btn waves-effect waves-light"><i class="material-icons">search</i></a></span></div></div></div>';
 
-            $("#sideRank>.row").apped(sideRank);
+            $("#sideRank>.row").append(sideRank);
             $("#sideRank>.row>.col:gt(4)").remove();
         }
     }
